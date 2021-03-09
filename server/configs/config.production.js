e08baae34c3e33dbcaf30963a6a4e180de222128
asribeiro.rafael <asribeiro.rafael@gmail.com>
2021-03-08 21:58:11 -0300

    - Production

--- server/configs/config.production.js
+++ server/configs/config.production.js
@@ -1,6 +1,6 @@
 module.exports = {
     Server: {
-        port: process.env.PORT || 31529
+        port: process.env.PORT || 8081
     },
     DB: {
         url: 'mongodb+srv://admin:certificadodev21@cluster0.wrrln.mongodb.net/'
